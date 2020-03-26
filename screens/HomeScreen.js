d9a0a64e7391f97e596821cffd0793ecfec9d9c9
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-25 22:16:11 -0400

    Landing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,25 +5,82 @@ import {
 	StyleSheet,
 	Text,
 	TouchableOpacity,
+	TextInput,
+	Button,
 	View,
 } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
+import Svg, { Ellipse } from 'react-native-svg';
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
-	return (
-		<View style={styles.container}>
-			<ScrollView
-				style={styles.container}
-				contentContainerStyle={styles.contentContainer}>
-				<View style={styles.welcomeContainer}>
-					<MonoText style={{ fontSize: 30 }}></MonoText>
+export default class HomeScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+		};
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.ellipse1Stack}>
+					<Svg viewBox='0 0 308.44 304.34' style={styles.ellipse1}>
+						<Ellipse
+							fill='rgba(97,111,253,0.78)'
+							cx={154}
+							cy={152}
+							rx={154}
+							ry={152}></Ellipse>
+					</Svg>
+					<Svg viewBox='0 0 353.62 344.41' style={styles.ellipse}>
+						<Ellipse
+							fill='rgba(97,111,253,1)'
+							cx={177}
+							cy={172}
+							rx={176}
+							ry={172}></Ellipse>
+					</Svg>
+					<View>
+						<MonoText style={styles.nameLabel}>
+							Enter your username
+						</MonoText>
+						<View style={styles.nameInput}>
+							<TextInput
+								style={{ color: '#D0D0C0' }}
+								onChangeText={username =>
+									this.setState({ username })
+								}
+								value={this.state.username}
+							/>
+						</View>
+						<MonoText style={styles.nameLabel1}>
+							Enter your password
+						</MonoText>
+						<View style={styles.nameInput1}>
+							<TextInput
+								style={{ color: '#D0D0C0' }}
+								onChangeText={password =>
+									this.setState({ password })
+								}
+								value={this.state.password}
+							/>
+						</View>
+						<View style={styles.submitButton}>
+							<Button
+								style={styles.submitButton}
+								title='SUBMIT'
+								onPress={() => this.nameSubmit(this.state.name)}
+							/>
+						</View>
+					</View>
 				</View>
-			</ScrollView>
-		</View>
-	);
+			</View>
+		);
+	}
 }
 
 HomeScreen.navigationOptions = {
@@ -31,90 +88,72 @@ HomeScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
+	nameLabel: {
+		top: 70,
+		left: 65,
+		fontSize: 23,
+		color: 'black',
+	},
+	submitButton: {
+		borderColor: 'black',
+		borderWidth: 1,
+		fontFamily: 'bubblegum',
+		color: '#fff',
+		backgroundColor: '#1E90FF',
+		borderRadius: 30,
+		top: 180,
+		left: 110,
+		width: 130,
+	},
+	nameInput: {
+		borderColor: 'black',
+		borderBottomWidth: 1,
+		color: '#D0D0C0',
+		fontSize: 20,
+		top: 75,
+		left: 80,
+		width: 200,
+		height: 30,
+	},
+	nameLabel1: {
+		top: 110,
+		left: 90,
+		fontSize: 23,
+		color: 'black',
+	},
+	nameInput1: {
+		borderColor: 'black',
+		borderBottomWidth: 1,
+		color: '#D0D0C0',
+		fontSize: 20,
+		top: 190,
+		left: 100,
+		width: 230,
+		height: 30,
+		position: 'absolute',
+	},
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
 	},
-	developmentModeText: {
-		marginBottom: 20,
-		color: 'rgba(0,0,0,0.4)',
-		fontSize: 14,
-		lineHeight: 19,
-		textAlign: 'center',
-	},
-	contentContainer: {
-		paddingTop: 30,
-	},
-	welcomeContainer: {
-		alignItems: 'center',
-		marginTop: 10,
-		marginBottom: 20,
-	},
-	welcomeImage: {
-		width: 100,
-		height: 80,
-		resizeMode: 'contain',
-		marginTop: 3,
-		marginLeft: -10,
-	},
-	getStartedContainer: {
-		alignItems: 'center',
-		marginHorizontal: 50,
-	},
-	homeScreenFilename: {
-		marginVertical: 7,
-	},
-	codeHighlightText: {
-		color: 'rgba(96,100,109, 0.8)',
-	},
-	codeHighlightContainer: {
-		backgroundColor: 'rgba(0,0,0,0.05)',
-		borderRadius: 3,
-		paddingHorizontal: 4,
-	},
-	getStartedText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		lineHeight: 24,
-		textAlign: 'center',
-	},
-	tabBarInfoContainer: {
+	ellipse1: {
+		top: 206,
+		left: 164,
+		width: 308,
+		height: 304,
 		position: 'absolute',
-		bottom: 0,
+	},
+	ellipse: {
+		top: 0,
 		left: 0,
-		right: 0,
-		...Platform.select({
-			ios: {
-				shadowColor: 'black',
-				shadowOffset: { width: 0, height: -3 },
-				shadowOpacity: 0.1,
-				shadowRadius: 3,
-			},
-			android: {
-				elevation: 20,
-			},
-		}),
-		alignItems: 'center',
-		backgroundColor: '#fbfbfb',
-		paddingVertical: 20,
+		width: 354,
+		height: 344,
+		position: 'absolute',
 	},
-	tabBarInfoText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		textAlign: 'center',
-	},
-	navigationFilename: {
-		marginTop: 5,
-	},
-	helpContainer: {
-		marginTop: 15,
-		alignItems: 'center',
-	},
-	helpLink: {
-		paddingVertical: 15,
-	},
-	helpLinkText: {
-		fontSize: 14,
-		color: '#2e78b7',
+
+	ellipse1Stack: {
+		width: 472,
+		height: 510,
+		marginTop: 117,
+		marginLeft: -57,
 	},
 });
