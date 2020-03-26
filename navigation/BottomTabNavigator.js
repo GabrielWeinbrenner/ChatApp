a3a15d1e8ca6066f43b2c11c5cb061122addeba7
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-25 21:17:06 -0400

    Homescreen

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -3,45 +3,53 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
+import { MonoText } from '../components/StyledText';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
 export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+	// Set the header title on the parent stack navigator depending on the
+	// currently active tab. Learn more in the documentation:
+	// https://reactnavigation.org/docs/en/screen-options-resolution.html
+	navigation.setOptions({
+		headerTitle: <MonoText>{getHeaderTitle(route)}</MonoText>,
+	});
 
-  return (
-    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+	return (
+		<BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+			<BottomTab.Screen
+				name='Home'
+				component={HomeScreen}
+				options={{
+					title: 'Bubble Chat',
+					tabBarIcon: ({ focused }) => (
+						<TabBarIcon focused={focused} name='md-code-working' />
+					),
+				}}
+			/>
+			<BottomTab.Screen
+				name='Links'
+				component={LinksScreen}
+				options={{
+					title: 'Profile',
+					tabBarIcon: ({ focused }) => (
+						<TabBarIcon focused={focused} name='md-book' />
+					),
+				}}
+			/>
+		</BottomTab.Navigator>
+	);
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+	const routeName =
+		route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
-  switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
-  }
+	switch (routeName) {
+		case 'Home':
+			return 'Welcome to Bubble Chat';
+		case 'Links':
+			return 'Profile';
+	}
 }
